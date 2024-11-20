fe80d66095fc6674699e9a9e85d50e294386dd85
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-11-20 17:36:50 +0900

    [Feat]: 프로필 동기화
    본인 프로필 표시하기
    아직 동기화 완벽 구현 X

---
+++ Assets/LeeYunJeong/Scripts/PlayerProfileManager4.cs
@@ -0,0 +1,55 @@
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerProfileManager4 : MonoBehaviourPunCallbacks
+{
+    [SerializeField] GameObject[] profileCards;
+    [SerializeField] Image[] profileImages;
+    [SerializeField] TMP_Text[] scoreTexts;
+    [SerializeField] TMP_Text[] hpTexts;
+    [SerializeField] Color myProfileColor = Color.red;
+
+    private int playerHP = 100;
+
+    private void Start()
+    {
+        foreach (var card in profileCards)
+        {
+            card.SetActive(false);
+        }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("방입장");
+        InitializeProfileCards();
+    }
+
+
+    private void InitializeProfileCards()
+    {
+        int playerCount = PhotonNetwork.PlayerList.Length;
+
+        //플레이어 수에 맞게 카드 활성화
+        for (int i = 0; i < 4; i++)
+        {
+            profileCards[i].SetActive(i < playerCount);
+        }
+
+        //본인 프로필 카드 하이라이트
+        for (int i = 0; i < playerCount; i++)
+        {
+            if (PhotonNetwork.LocalPlayer.ActorNumber == PhotonNetwork.PlayerList[i].ActorNumber)
+            {
+                profileCards[i].GetComponent<Image>().color = myProfileColor;
+            }
+        }
+    }
+
+    public void UpdateProfileInfo(int score, int hp)
+    {
+        //TODO: 점수, HP 관련추가
+    }
+}
