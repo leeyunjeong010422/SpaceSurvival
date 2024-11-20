614e48dccd3a0c3452db6156314edb993481c1d8
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-11-20 12:45:43 +0900

    [Feat]: 플레이어 총알 발사

---
+++ Assets/LeeYunJeong/Scripts/Bullet4.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Bullet4 : MonoBehaviour
+{
+    [SerializeField] float speed = 20f;
+    [SerializeField] float lifetime = 2f;
+    [SerializeField] int damage = 10;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        rb.velocity = transform.forward * speed;
+
+        Destroy(gameObject, lifetime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/LeeYunJeong/Scripts/PlayerController4.cs
+++ Assets/LeeYunJeong/Scripts/PlayerController4.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController4 : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] Transform muzzlePoint;
+    [SerializeField] GameObject bulletPrefab;
 
     private void Update()
     {
         Move();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
     }
 
     private void Move()
@@ -22,5 +30,10 @@ public class PlayerController4 : MonoBehaviour
 
         Vector3 worldMoveDir = transform.TransformDirection(moveDir).normalized;
         transform.position += worldMoveDir * speed * Time.deltaTime;
+    }
+
+    private void Fire()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, muzzlePoint.position, muzzlePoint.rotation);
     }
 }
