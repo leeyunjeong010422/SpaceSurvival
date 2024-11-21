1e4686a6a8390a567903d4eb6b774d2ab4541362
LeeJeongbin <ljb97310@naver.com>
2024-11-21 11:49:05 +0900

    [Delete] 스포너 기능으로 자동 추가

--- Assets/LeeJeongBin/Scripts/AISpawner3.cs
+++ Assets/LeeJeongBin/Scripts/AISpawner3.cs
@@ -11,11 +11,10 @@ public class AISpawner3 : MonoBehaviourPunCallbacks
     public int AICount;
     public float spawnAreaSize;
 
-    private HashSet<Vector3> validPosition = new HashSet<Vector3>(); // 중복 생성을 피하기 위함
+    private HashSet<Vector3> validPosition = new HashSet<Vector3>();
 
     void Start()
     {
-        // 마스터 클라이언트만 AI 소환
         if (PhotonNetwork.IsMasterClient)
         {
             SpawnAI(AICount);
@@ -32,7 +31,6 @@ public class AISpawner3 : MonoBehaviourPunCallbacks
         for (int i = 0; i < count; i++)
         {
             Vector3 spawnPosition = GetRandomSpawnPosition();
-            // PhotonNetwork.Instantiate를 사용하여 네트워크에서 AI를 생성
             PhotonNetwork.Instantiate(AIPrefab.name, spawnPosition, Quaternion.identity);
         }
     }
@@ -40,7 +38,7 @@ public class AISpawner3 : MonoBehaviourPunCallbacks
     Vector3 GetRandomSpawnPosition()
     {
         Vector3 randomPosition = Vector3.zero;
-        bool validPosition = false; // 위치가 유효한지
+        bool validPosition = false;
         int attempts = 0;
 
         while (!validPosition && attempts < 5)
@@ -61,13 +59,10 @@ public class AISpawner3 : MonoBehaviourPunCallbacks
         return validPosition ? randomPosition : Vector3.zero;
     }
 
-    // 클라이언트가 네트워크로부터 동기화된 AI 정보를 받을 때, AI의 상태를 업데이트하는 메서드 (예시)
     public override void OnJoinedRoom()
     {
-        // 이 메서드는 모든 클라이언트에서 호출됩니다.
         if (!PhotonNetwork.IsMasterClient)
         {
-            // AI 생성과 관련된 작업을 마스터 클라이언트가 처리하도록 하므로
             Debug.Log("AI가 생성되었습니다.");
         }
     }
