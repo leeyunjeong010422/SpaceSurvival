5204950c2af3dc19a4c1b01ee2393f951678f6a6
LeeJeongbin97 <ljb97310@naver.com>
2024-11-26 17:16:22 +0900

    [Feat] 체크포인트 주석 추가

--- Assets/LeeJeongBin/Scripts/CheckPoint3.cs
+++ Assets/LeeJeongBin/Scripts/CheckPoint3.cs
@@ -1,12 +1,12 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CheckPoint3 : MonoBehaviour
+public class CheckPoint3 : MonoBehaviourPun
 {
     public static CheckPoint3 Instance;
     public int TotalCheckPoints { get; private set; } = 4;
-    public static List<CheckPoint3> visitedCheckPoints = new List<CheckPoint3>();
 
     private void Awake()
     {
@@ -21,17 +21,24 @@ public class CheckPoint3 : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             PlayerController3 playerController = other.GetComponent<PlayerController3>();
-            if (playerController != null && !visitedCheckPoints.Contains(this))
+
+            if (playerController != null && !PlayerController3.visitedCheckPoint.Contains(this))
             {
-                Debug.Log($"플레이어가 체크포인트 {gameObject.name}에 도달했습니다");
+                Debug.Log($"플레이어가 체크포인트 {gameObject.name}에 도달했습니다.");
                 playerController.OnTriggerCheckPoint(this);
-                visitedCheckPoints.Add(this);
+
+                PlayerController3.visitedCheckPoint.Add(this);
+
+                playerController.IncrementCheckPointsReached();
+                Debug.Log($"현재 체크포인트 통과 : {playerController.CheckPointsReached}");
+
+                if (playerController.CheckPointsReached >= TotalCheckPoints)
+                {
+                    // 모든 체크포인트를 통과한 경우
+                    Debug.Log("모든 체크포인트를 통과하여 승리하였습니다.");
+                    // 게임 완료 또는 승리 로직 추가 해야함
+                }
             }
         }
-    }
-
-    public static void ResetCheckPoints()
-    {
-        visitedCheckPoints.Clear();
     }
 }
