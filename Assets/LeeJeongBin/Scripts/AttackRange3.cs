b248cefae4f2e475c437f381d4d1845de497276c
februarywind <89524114+februarywind@users.noreply.github.com>
2024-11-27 16:35:47 +0900

    [Fix] 어택 래인지 기본 비활성화

--- Assets/LeeJeongBin/Scripts/AttackRange3.cs
+++ Assets/LeeJeongBin/Scripts/AttackRange3.cs
@@ -8,7 +8,6 @@ public class AttackRange3 : MonoBehaviour
     [SerializeField] Killing3 killing3;
     private void OnTriggerEnter(Collider other)
     {
-
         killing3.HandleTargetDeath(other.gameObject);
     }
 }
