9b24516a5b6ac9f44a5f22d1c7ab76e3a1947717
JeeSeong <rhtmxm17@gmail.com>
2024-11-28 14:28:24 +0900

    [Feat]: 사운드 매니저에 오디오 믹서 음량 설정 추가

--- Assets/Common/Script/SoundManager.cs
+++ Assets/Common/Script/SoundManager.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
+public enum AudioGroup { MASTER, BGM, SFX }
+
 public class SoundManager : SingletonBehaviour<SoundManager>
 {
     [SerializeField] AudioMixer mixer;
     [SerializeField] AudioSource bgmSource;
     [SerializeField] AudioSource sfxSource;
 
+    private static readonly string[] paramNames = { "Master", "BGM", "SFX" };
+
     private void Awake()
     {
         RegisterSingleton(this);
     }
 
+    /// <summary>
+    /// BGM을 재생
+    /// </summary>
+    /// <param name="clip">BGM으로 재생할 오디오 클립</param>
+    /// <param name="volumeScale">음량 배율</param>
     public void PlayBGM(AudioClip clip, float volumeScale = 1f)
     {
         bgmSource.clip = clip;
@@ -26,6 +35,33 @@ public class SoundManager : SingletonBehaviour<SoundManager>
         bgmSource.Stop();
     }
 
-    // 3D가 아닌 효과음 출력 대행
+    /// <summary>
+    /// 효과음을 재생, 거리의 영향을 받는 효과음은 매니저를 통하지 말고 따로 재생 필요
+    /// </summary>
+    /// <param name="clip">재생할 오디오 클립</param>
+    /// <param name="volumeScale">음량 배율</param>
     public void PlaySFX(AudioClip clip, float volumeScale = 1f) => sfxSource.PlayOneShot(clip, volumeScale);
+
+    /// <summary>
+    /// 해당 오디오 그룹에 대한 전역 음량 스케일을 설정<br/>
+    /// Playe 메서드의 매개변수 volumeScale과는 별개
+    /// </summary>
+    /// <param name="group">그룹</param>
+    /// <param name="scale">음량</param>
+    public void SetMixerScale(AudioGroup group, float scale)
+    {
+        if (false == mixer.SetFloat(paramNames[(int)group], scale))
+        {
+            Debug.LogWarning("잘못된 AudioGroup");
+        }
+    }
+
+    public float GetMixerScale(AudioGroup group)
+    {
+        if (false == mixer.GetFloat(paramNames[(int)group], out float value))
+        {
+            Debug.LogWarning("잘못된 AudioGroup");
+        }
+        return value;
+    }
 }
