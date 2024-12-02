4240cda8b9c8052abbf61d649629dbc53082cc99
JeeSeong <rhtmxm17@gmail.com>
2024-12-02 17:54:45 +0900

    [Feat]: 로그인 한 디바이스id 확인

--- Assets/KwonMingyu/Script/Matchmaking/LoginPanel1.cs
+++ Assets/KwonMingyu/Script/Matchmaking/LoginPanel1.cs
@@ -1,3 +1,4 @@
+using Firebase.Database;
 using Firebase.Extensions;
 using Photon.Pun;
 using Photon.Realtime;
@@ -37,6 +38,14 @@ public class LoginPanel1 : MonoBehaviour
                 GameManager.UserSetting.SaveSetting();
 
                 // TODO: 중복 로그인 방지 기능
+                BackendManager1.Instance.userUidDataRef = BackendManager1.Database.RootReference.Child("UserData").Child(BackendManager1.Auth.CurrentUser.UserId);
+
+                // 현재 로그인한 디바이스를 DB에 등록 후 값 변경시 비교
+                DatabaseReference deviceDataRef = GameManager.Backend.userUidDataRef.Child("Device");
+                deviceDataRef.SetValueAsync(SystemInfo.deviceUniqueIdentifier);
+                deviceDataRef.ValueChanged += TEST_DeviceDataRef_ValueChanged;
+
+
                 // 게임 진행은 문제가 없지만 닉네임을 서버에서 받기 때문에
                 Debug.Log("로그인 성공 마스터 서버로 연결");
                 PhotonNetwork.ConnectUsingSettings();
@@ -48,6 +57,14 @@ public class LoginPanel1 : MonoBehaviour
             }
         });
     }
+
+    static private void TEST_DeviceDataRef_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        Debug.Log($"current:{SystemInfo.deviceUniqueIdentifier}, old:{e.Snapshot.Value.ToString()}");
+    }
+
+    static 
+
     public void Quit()
     {
 #if UNITY_EDITOR
